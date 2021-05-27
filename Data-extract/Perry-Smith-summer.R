831b5d85c3162ff69be3b87750f38f032b48c283
ricardd <daniel.ricard@gmail.com>
2021-05-26 21:55:43 -0300

    add environmental preference single figure

--- Data-extract/Perry-Smith-summer.R
+++ Data-extract/Perry-Smith-summer.R
@@ -40,6 +40,10 @@ summer.tows.df <- subset(
      STRAT == '490' | STRAT == '491' | STRAT == '492' | STRAT == '493' | STRAT == '494' | STRAT == '495' ) & (MONTH == 6 | MONTH == 7 | MONTH == 8)
 )
 
+
+###########################################
+## DEPTH
+###########################################
 ## depth
 table(is.na(summer.tows.df$DMIN))
 table(is.na(summer.tows.df$DMAX))
@@ -103,6 +107,10 @@ for(t in depths){
   cdf.df[cdf.df$depth==t,"cdf"] <- this.depth.cdf
   
 }## end loop over depths
+
+depth.cdf <- cdf.df
+depth.cdf$variable="Depth"
+names(depth.cdf)[1] <- "variable.value"
 
 out.df <- cdf.df
 names(out.df) <- c("depth","cdf.depth")
@@ -200,16 +208,165 @@ g
 ## this is pointless for depth, there are no differences between decades
 
 
-## bottom temperature
-table(is.na(summer.tows.df$BOTTOM_TEMPERATURE))
+
+###########################################
+## BOTTOM TEMPERATURE
+###########################################
+temperature.df <- summer.tows.df[which(!is.na(summer.tows.df$BOTTOM_TEMPERATURE)),]
+
+st <- read.csv(file.path(actualreport.path, "strata-statistics.csv"))
+st <- st[which(st$STRAT %in% c(440:466, 470:478, 480:485, 490:495)),]
+
+x <- temperature.df
+x$unique.id <- paste0(x$MISSION, "-", x$SETNO)
+
+##
+## following Table 1 in Perry and Smith (1994)
+# nh = number of hauls or sets in stratum h (h = 1, . . ., L)
+nh <- aggregate(unique.id~STRAT, data=x, length)
+st <- merge(st, nh, by="STRAT")
+st$nh <- st$unique.id
 
 
-## bottom salinty
-table(is.na(summer.tows.df$BOTTOM_SALINITY))
+## n = total number of hauls
+n <- nrow(x)
+
+# Nh, = total number of possible sets in stratum h
+# a.k.a. number of trawlable units
+# swept area of a 30-minute WIIa set, 
+swept.area <- 4E-02 ## NOT THE REAL VALUE, check this
+st$Nh <- st$AREA / swept.area
+
+# N = total number of possible sets overall
+N <- sum(st$Nh)
+
+# Wh = proportion of the survey area in stratum h
+st$Wh <- st$AREA / sum(st$AREA)
+## in Perry and Smith, the proportion is computed as Nh/N 
+## (st$Wh - st$Nh / N) < 1E-12 ## the numbers numerically differ in R, but they are the same
+
+temperatures <- seq(min(x$BOTTOM_TEMPERATURE), max(x$BOTTOM_TEMPERATURE), length.out=200)
+
+cdf.df <- expand.grid(temperature=temperatures, cdf=NA) ## CDF for sets
+
+for(t in temperatures){
+  this.temperature.cdf <- 0
+  
+  ## loop over strata
+  for(h in unique(x$STRAT)){
+    ## number of unique tows in this stratum
+    t.df <- x[x$STRAT==h,]
+    ## loop over sets in each stratum
+    for(i in 1:nrow(t.df)){
+      this.set.cdf <- ifelse(t.df[i,"BOTTOM_TEMPERATURE"]<=t,1,0) * (st[st$STRAT==h,"Wh"] / st[st$STRAT==h,"nh"])
+      this.temperature.cdf <- this.temperature.cdf + this.set.cdf
+    }## end loop over sets
+  }## end loop over strata
+  
+  
+  cdf.df[cdf.df$temperature==t,"cdf"] <- this.temperature.cdf
+  
+}## end loop over temperatures
+
+temp.cdf <- cdf.df
+temp.cdf$variable="Bottom temperature"
+names(temp.cdf)[1] <- "variable.value"
+out.df <- cdf.df
+names(out.df) <- c("temperature","cdf.temperature")
+
+g <- ggplot(out.df, aes(x=temperature, y=cdf.temperature)) +
+  geom_line() +
+  theme_bw()
+g
+
+ggsave(g, file=file.path(actualreport.path, "figures/summer-temperature-CDF.png"))
+
+
+
+###########################################
+## BOTTOM SALINITY
+###########################################
+salinity.df <- summer.tows.df[which(!is.na(summer.tows.df$BOTTOM_SALINITY) & summer.tows.df$BOTTOM_SALINITY>0),]
+
+
+st <- read.csv(file.path(actualreport.path, "strata-statistics.csv"))
+st <- st[which(st$STRAT %in% c(440:466, 470:478, 480:485, 490:495)),]
+
+x <- salinity.df
+x$unique.id <- paste0(x$MISSION, "-", x$SETNO)
+
+##
+## following Table 1 in Perry and Smith (1994)
+# nh = number of hauls or sets in stratum h (h = 1, . . ., L)
+nh <- aggregate(unique.id~STRAT, data=x, length)
+st <- merge(st, nh, by="STRAT")
+st$nh <- st$unique.id
+
+
+## n = total number of hauls
+n <- nrow(x)
+
+# Nh, = total number of possible sets in stratum h
+# a.k.a. number of trawlable units
+# swept area of a 30-minute WIIa set, 
+swept.area <- 4E-02 ## NOT THE REAL VALUE, check this
+st$Nh <- st$AREA / swept.area
+
+# N = total number of possible sets overall
+N <- sum(st$Nh)
+
+# Wh = proportion of the survey area in stratum h
+st$Wh <- st$AREA / sum(st$AREA)
+## in Perry and Smith, the proportion is computed as Nh/N 
+## (st$Wh - st$Nh / N) < 1E-12 ## the numbers numerically differ in R, but they are the same
+
+salinities <- seq(min(x$BOTTOM_SALINITY), max(x$BOTTOM_SALINITY), length.out=200)
+
+cdf.df <- expand.grid(salinity=salinities, cdf=NA) ## CDF for sets
+
+for(t in salinities){
+  this.salinity.cdf <- 0
+  
+  ## loop over strata
+  for(h in unique(x$STRAT)){
+    ## number of unique tows in this stratum
+    t.df <- x[x$STRAT==h,]
+    ## loop over sets in each stratum
+    for(i in 1:nrow(t.df)){
+      this.set.cdf <- ifelse(t.df[i,"BOTTOM_SALINITY"]<=t,1,0) * (st[st$STRAT==h,"Wh"] / st[st$STRAT==h,"nh"])
+      this.salinity.cdf <- this.salinity.cdf + this.set.cdf
+    }## end loop over sets
+  }## end loop over strata
+  
+  
+  cdf.df[cdf.df$salinity==t,"cdf"] <- this.salinity.cdf
+  
+}## end loop over salinities
+
+sal.cdf <- cdf.df
+sal.cdf$variable="Bottom salinity"
+names(sal.cdf)[1] <- "variable.value"
+
+out.df <- cdf.df
+names(out.df) <- c("salinity","cdf.salinity")
+
+g <- ggplot(out.df, aes(x=salinity, y=cdf.salinity)) +
+  geom_line() +
+  theme_bw()
+g
+
+ggsave(g, file=file.path(actualreport.path, "figures/summer-salinity-CDF.png"))
 
 
 ## figure with overall cumulative distribution of depth, bottom temperature and bottom salinity
+all.df <- rbind(depth.cdf,temp.cdf,sal.cdf)
 
+g <- ggplot(all.df, aes(x=variable.value, y=cdf)) +
+  geom_line(aes(x=variable.value, y=cdf)) + 
+  facet_wrap(~variable, scales="free",ncol=1) +
+  theme_bw()
 
-## 
+g
+
+ggsave(g, file=file.path(actualreport.path, "figures/summer-depth-temperature-salinity-CDF.png"))
 
